5ee10c4004fd811cc708f8383ea0e91896b2eab0
Zinckar <zhakobin@gmail.com>
2017-11-09 19:16:38 +0200

    Add proper relations in DB between Identity and UserProfiles

--- MediaService.Database/dbo/Tables/UserProfiles.sql
+++ MediaService.Database/dbo/Tables/UserProfiles.sql
@@ -2,10 +2,14 @@
     [Id]       NVARCHAR (128) NOT NULL,
     [Nickname] NVARCHAR (128) NOT NULL,
     [Avatar]   NVARCHAR (60)  NULL,
+    [UserName] NVARCHAR (256) NULL,
     CONSTRAINT [PK_dbo.UserProfiles] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_dbo.UserProfiles_dbo.AspNetUsers_Id] FOREIGN KEY ([Id]) REFERENCES [dbo].[AspNetUsers] ([Id]),
-    CONSTRAINT AK_Nickname UNIQUE([Nickname]) 
+    CONSTRAINT [FK_UserProfiles_AspNetUsers] FOREIGN KEY ([UserName]) REFERENCES [dbo].[AspNetUsers] ([UserName]),
+    CONSTRAINT [AK_Nickname] UNIQUE NONCLUSTERED ([Nickname] ASC),
+    CONSTRAINT [IX_UserProfiles] UNIQUE NONCLUSTERED ([UserName] ASC)
 );
+
+
 
 
 GO
