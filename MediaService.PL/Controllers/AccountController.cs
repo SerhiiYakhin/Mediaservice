6021ea89f0a64391ffcd6e285aaaa22f2d9e3587
Zinckar <zhakobin@gmail.com>
2017-11-10 12:01:15 +0200

    basic general views

--- MediaService.PL/Controllers/AccountController.cs
+++ MediaService.PL/Controllers/AccountController.cs
@@ -227,7 +227,7 @@ namespace MediaService.PL.Controllers
                         // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                         // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                         // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
+                        ViewBag.FirstTime = "true";
                         return RedirectToAction("Index", "Home");
                     }
 
